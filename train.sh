c1ff4f18a26f3c1b0012acaef7824277c25071e9
Jiaju-Chen <87610757+Jiaju-Chen@users.noreply.github.com>
2024-09-02 21:41:15 +0800

    Add files via upload

--- train.sh
+++ train.sh
@@ -1,4 +1,9 @@
 export NCCL_IB_DISABLE=1
+## Note: change '--train_data_path --val_data_path --output_dir' for different tasks
+## For example, use different training sets for each task:
+## For task GP, train_1000_GP.json
+## For task GF, train_1000_BERT_GF.json, train_1000_BERT_GFd.json, train_1000_BERT_GFn.json (the latter two for data augmentation)
+## For task IP, train_1000_BERT_IP.json, train_1000_BERT_IPd.json, train_1000_BERT_IPn.json (the latter two for data augmentation)
 
 dataset='movie_sgcate'
 for seed in 1
@@ -10,10 +15,10 @@ do
             for sample in -1
             do
                 echo "lr: $lr, dropout: $dropout, seed: $seed,"
-                python train_toy_save1.py \
+                python train.py \
                     --base_model "../Meta-Llama-3-8B-Instruct/" \
-                    --train_data_path "['./data/$dataset/train_1000_BERT_GP.json']"   \
-                    --val_data_path "['./data/$dataset/valid_1000_BERT_GP.json']" \
+                    --train_data_path "['./data/$dataset/train_1000_GP.json']"   \
+                    --val_data_path "['./data/$dataset/valid_1000_GP.json']" \
                     --output_dir ./model/${dataset}/BERT_GP_vlGP_ep25_${lr} \
                     --batch_size 250  \
                     --micro_batch_size 4 \
