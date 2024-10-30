e8aad2ec0512f80f48b2cdc4bf13d4b1883d631f
陈家驹 <87610757+Jiaju-Chen@users.noreply.github.com>
2024-10-30 19:49:09 +0800

    Add files via upload

--- train.sh
+++ train.sh
@@ -2,9 +2,12 @@ export NCCL_IB_DISABLE=1
 ## Note: change '--train_data_path --val_data_path --output_dir' for different tasks
 ## For example, use different training sets for each task:
 ## For task GP, train_1000_GP.json
-## For task GF, train_1000_BERT_GF.json, train_1000_BERT_GFd.json, train_1000_BERT_GFn.json (the latter two for data augmentation)
-## For task IP, train_1000_BERT_IP.json, train_1000_BERT_IPd.json, train_1000_BERT_IPn.json (the latter two for data augmentation)
-
+## For task GF, train_1000_BERT_GF.json, train_1000_BERT_GF_d.json, train_1000_BERT_GF_n.json (the latter two for data augmentation)
+## For task IP, train_1000_BERT_IP.json, train_1000_BERT_IP_d.json, train_1000_BERT_IP_n.json (the latter two for data augmentation)
+## For example, when we train task IP along with augmented data, we change the following sentences:
+                    # --train_data_path "['./data/$dataset/train_1000_BERT_IP.json', './data/$dataset/train_1000_BERT_IP_d.json', './data/$dataset/train_1000_BERT_IP_n.json']"   \
+                    # --val_data_path "['./data/$dataset/train_1000_BERT_IP.json']" \
+                    # --output_dir ./model/${dataset}/BERT_IPIPdIPn_vlIP_ep25_${lr} \
 dataset='movie_sgcate'
 for seed in 1
 do
@@ -17,12 +20,12 @@ do
                 echo "lr: $lr, dropout: $dropout, seed: $seed,"
                 python train.py \
                     --base_model "../Meta-Llama-3-8B-Instruct/" \
-                    --train_data_path "['./data/$dataset/train_1000_GP.json']"   \
-                    --val_data_path "['./data/$dataset/valid_1000_GP.json']" \
-                    --output_dir ./model/${dataset}/BERT_GP_vlGP_ep25_${lr} \
+                    --train_data_path "['./data/$dataset/train_1000_BERT_IP.json', './data/$dataset/train_1000_BERT_IP_d.json']"   \
+                    --val_data_path "['./data/$dataset/train_1000_BERT_IP.json']" \
+                    --output_dir ./model/${dataset}/BERT_IPIPd_vlIP_ep25_${lr} \
                     --batch_size 250  \
-                    --micro_batch_size 4 \
-                    --num_epochs 25 \
+                    --micro_batch_size 2 \
+                    --num_epochs 5 \
                     --learning_rate $lr \
                     --cutoff_len 768 \
                     --lora_r 8 \
